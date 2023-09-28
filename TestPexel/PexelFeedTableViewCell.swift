edd69341f1737dae4e38491819c4239dd1833019
Serhii Oleinich <solei@dolby.com>
2023-09-28 12:33:01 +0300

    Add base UI

---
+++ TestPexel/PexelFeedTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  PexelFeedTableViewCell.swift
+//  TestPexel
+//
+//  Created by Serhii Oleinich on 28.09.2023.
+//
+
+import UIKit
+
+final class PexelFeedTableViewCell: UITableViewCell {
+    @IBOutlet private weak var title: UILabel!
+    @IBOutlet private weak var icon: UIImageView!
+    @IBOutlet private weak var feedImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        title.text = "Test"
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        title.text = nil
+        icon.image = nil
+        feedImage.image = nil
+    }
+}
--- TestPexel/PexelFeedViewController.swift
+++ TestPexel/PexelFeedViewController.swift
@@ -8,7 +8,28 @@
 import UIKit
 
 final class PexelFeedViewController: UIViewController {
+    @IBOutlet private weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension PexelFeedViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int { 1 }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { 10 }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: PexelFeedTableViewCell.self), for: indexPath) as? PexelFeedTableViewCell else {
+            preconditionFailure("Could not get cell")
+        }
+        
+        return cell
     }
 }
