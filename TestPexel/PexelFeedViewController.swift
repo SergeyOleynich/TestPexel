ae4c4042a04f6641f500a5904a75032287924233
Serhii Oleinich <solei@dolby.com>
2023-09-28 11:50:25 +0300

    Add Feed View Controller

---
+++ TestPexel/PexelFeedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PexelFeedViewController.swift
+//  TestPexel
+//
+//  Created by Serhii Oleinich on 28.09.2023.
+//
+
+import UIKit
+
+final class PexelFeedViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
